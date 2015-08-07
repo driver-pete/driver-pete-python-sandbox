9a37cf11246ccb89cbb38e19b2e826d4b7ce4e5c
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-08-07 07:09:57 -0700

    trak reader

--- driver_pete_python_sandbox/test_trajectory_reader.py
+++ driver_pete_python_sandbox/test_trajectory_reader.py
@@ -1,10 +1,16 @@
 
-from boto.s3.connection import S3Connection
+import os
+import tempfile
+from driver_pete_python_sandbox.download import S3
+from driver_pete_python_sandbox.trajectory_reader import read_compressed_trajectory,\
+    write_compressed_trajectory
 
 
 def test_trajectory_reader():
-    pass
-
-
-if __name__ == "__main__":
-    pass
+    folder = tempfile.mkdtemp()
+    s3 = S3('driverpete-storage')
+    filename = s3.download("_testing/testing_merged_0", folder)
+    trajectory = read_compressed_trajectory(filename)
+    write_compressed_trajectory(trajectory, os.path.join(folder, 'trajectory_copy'))
+    trajectory_copy = read_compressed_trajectory(os.path.join(folder, 'trajectory_copy'))
+    assert((trajectory == trajectory_copy).all())

4f0ce05e93750ee474a02c657c6d877aa0fb890f
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-08-09 08:08:22 -0700

    working tests

--- driver_pete_python_sandbox/test_filter_gps.py
+++ driver_pete_python_sandbox/test_filter_gps.py
@@ -6,29 +6,35 @@
 # ============================================================================
 
 
-import numpy as np
 import pprint
- 
-from driver_pete_python_sandbox.trajectory_reader import read_compressed_trajectory
 import tempfile
-from driver_pete_python_sandbox.download import S3
-from driver_pete_python_sandbox.filter_gps import compute_velocities,\
-    remove_duplicate_points, ms_to_mph, remove_outliers, filter_gps_data,\
-    extract_delta_time, extract_delta_dist
-from driver_pete_python_sandbox.filter_gps_processor import apply_filter,\
-    DuplicateTimeFilter, VelocityOutliersFilter
-from driver_pete_python_sandbox.gmaps import trajectory_point_to_str
+
 from geopy.distance import vincenty
- 
- 
+
+from driver_pete_python_sandbox.download import S3
+from driver_pete_python_sandbox.filter_gps import compute_velocities, \
+    remove_duplicate_points, ms_to_mph, \
+    extract_delta_time, extract_delta_dist, remove_stationary_points
+from driver_pete_python_sandbox.filter_gps_processor import apply_filter, \
+    DuplicateTimeFilter, VelocityOutliersFilter, filter_gps_data
+from driver_pete_python_sandbox.gmaps import trajectory_point_to_str
+from driver_pete_python_sandbox.trajectory_reader import read_compressed_trajectory
+import numpy as np
+
+
 def _get_test_data():
     folder = tempfile.mkdtemp()
     s3 = S3('driverpete-storage')
     filename = s3.download("_testing/testing_raw_0", folder)
     data = read_compressed_trajectory(filename)
-    # add few duplicates
+    # add few time duplicates
     data[54] = data[53]
     data[100] = data[99]
+    
+    # add few distance duplicates
+    data[23][1:] = data[22][1:]
+    data[40][1:] = data[39][1:]
+    data[60][1:] = data[59][1:]
     return data
  
  
@@ -41,6 +47,9 @@ def test_remove_duplicate_readings():
     fixed_velocities = compute_velocities(fixed_data)
     assert(np.sum(np.isnan(fixed_velocities)) == 0)
     # check that we deleted duplicates only
+    print(data.shape[0])
+    print(fixed_data.shape[0])
+    print(number_of_duplicates)
     assert(fixed_data.shape[0] == data.shape[0] - number_of_duplicates)
     
     fixed_data_processor = apply_filter(data, DuplicateTimeFilter())
@@ -49,7 +58,7 @@ def test_remove_duplicate_readings():
  
 def test_remove_outliers():
     np.set_printoptions(suppress=True)
-    data = remove_duplicate_points(_get_test_data())
+    data = remove_stationary_points(remove_duplicate_points(_get_test_data()))
  
     velocity_threshold = 85.
     fixed_data = apply_filter(data, VelocityOutliersFilter(velocity_threshold))
@@ -63,7 +72,7 @@ def test_remove_outliers():
     # no large velocities left
     velocities = compute_velocities(fixed_data) 
     assert(np.amax(velocities)*ms_to_mph < velocity_threshold)
-    assert(np.amax(extract_delta_dist(fixed_data)) < 316)
+    assert(np.amax(extract_delta_dist(fixed_data)) < 330)
     
     # we expect 5 point to be removed
     assert(data.shape[0] - fixed_data.shape[0] == 5)
@@ -107,12 +116,12 @@ def test_remove_stationary_noise_return_to_stable():
 def test_filter_gps():
     original_data = _get_test_data()
     data = filter_gps_data(original_data)
-    assert(len(original_data)-len(data) == 9)
+    assert(len(original_data)-len(data) == 11)
      
     
 if __name__ == '__main__':
-    #test_remove_duplicate_readings()
+    test_remove_duplicate_readings()
     test_remove_outliers()
-    #test_filter_gps()
-    #test_remove_stationary_noise()
-    #test_remove_stationary_noise_return_to_stable()
+    test_remove_stationary_noise()
+    test_remove_stationary_noise_return_to_stable()
+    test_filter_gps()
